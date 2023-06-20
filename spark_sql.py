565ee2fb6d19211b7c683efab43a119b18a91dd0
Ovadia Ronen <edssron@gmail.com>
2023-06-20 18:18:00 +0000

    kafka

--- spark_sql.py
+++ spark_sql.py
@@ -1,92 +1,18 @@
 from pyspark.sql import SparkSession
 from pyspark.sql import functions as F
-from pyspark.sql.types import IntegerType, StringType, StructType, StructField
 
 # Local paths
-local_path_refine_output = "/home/naya/anomaly/files_parquet/scd_refine.parquet"
-local_path_anomaly_output = "/home/naya/anomaly/files_parquet/scd_anomaly.parquet"
-local_path_weeks_raws_output = "/home/naya/anomaly/files_parquet/scd_weeks_raws.parquet"
+local_path_refine_output = "/home/naya/anomaly/files_json/scd_refine.json"
+local_path_anomaly_output = "/home/naya/anomaly/files_json/scd_anomaly.json"
+local_path_weeks_raws_output = "/home/naya/anomaly/files_json/scd_weeks_raws.json"
 
 def spark_refine(df):
     try:
         # Create a SparkSession
         spark = SparkSession.builder.appName("Local_write_and_query").getOrCreate()
 
-        # Define the schema based on the provided column names and types
-        schema = StructType([
-            StructField("_BatchID", IntegerType(), True),
-            StructField("BatchName", StringType(), True),
-            StructField("BatchPath", StringType(), True),
-            StructField("ConfigID", IntegerType(), True),
-            StructField("Shift", StringType(), True),
-            StructField("Cell_Name", StringType(), True),
-            StructField("TestTypeID", IntegerType(), True),
-            StructField("LastInsertedRow", StringType(), True),
-            StructField("StartTime", StringType(), True),
-            StructField("Heads", StringType(), True),
-            StructField("BatteryTypeID", IntegerType(), True),
-            StructField("BodyTypeID", IntegerType(), True),
-            StructField("DomeTypeID", IntegerType(), True),
-            StructField("PCATypeID", IntegerType(), True),
-            StructField("PCBTypeID", IntegerType(), True),
-            StructField("PlacementContractorID", IntegerType(), True),
-            StructField("EmployeID", IntegerType(), True),
-            StructField("OpticsTypeID", IntegerType(), True),
-            StructField("Versions", StringType(), True),
-            StructField("LotNumber", StringType(), True),
-            StructField("SubLot", StringType(), True),
-            StructField("TestID", IntegerType(), True),
-            StructField("Capsule_Number", IntegerType(), True),
-            StructField("Initial_Capsule_Id_HEX", StringType(), True),
-            StructField("Capsule_Id_HEX", StringType(), True),
-            StructField("Test_Counter", IntegerType(), True),
-            StructField("ERROR_Code_Number", IntegerType(), True),
-            StructField("Capsule_Id", StringType(), True),
-            StructField("PASS_FAILED", IntegerType(), True),
-            StructField("Start_Time", StringType(), True),
-            StructField("End_time", StringType(), True),
-            StructField("Test_Time_min", StringType(), True),
-            StructField("Date", StringType(), True),
-            StructField("History", StringType(), True),
-            StructField("Remark", StringType(), True),
-            StructField("Blister_Id", IntegerType(), True),
-            StructField("Fail_Tray", StringType(), True),
-            StructField("Fail_capsule_position", IntegerType(), True),
-            StructField("ERROR_Code_Description", StringType(), True),
-            StructField("WTC", StringType(), True),
-            StructField("CQC_num", StringType(), True),
-            StructField("Capsule_Counter", IntegerType(), True),
-            StructField("TP_Cell_Name", StringType(), True),
-            StructField("DomeCaseGap", StringType(), True),
-            StructField("LaserPower", StringType(), True),
-            StructField("StitchArea", StringType(), True),
-            StructField("MinStitchWidth", StringType(), True),
-            StructField("LeakTest", StringType(), True),
-            StructField("year", IntegerType(), True),
-            StructField("quarter", IntegerType(), True),
-            StructField("week", IntegerType(), True),
-            StructField("month", IntegerType(), True),
-            StructField("dayweek", IntegerType(), True),
-            StructField("DomeCaseGap_spc_orig", StringType(), True),
-            StructField("LaserPower_spc_orig", StringType(), True),
-            StructField("MinStitchWidth_spc_orig", StringType(), True),
-            StructField("StitchArea_spc_orig", StringType(), True),
-            StructField("LeakTest_spc_orig", StringType(), True),
-            StructField("DomeCaseGap_spc", StringType(), True),
-            StructField("LaserPower_spc", StringType(), True),
-            StructField("MinStitchWidth_spc", StringType(), True),
-            StructField("StitchArea_spc", StringType(), True),
-            StructField("LeakTest_spc", StringType(), True),
-            StructField("BatchId", IntegerType(), True),
-            StructField("DomeCaseGap_limit", StringType(), True),
-            StructField("LaserPower_limit", StringType(), True),
-            StructField("MinStitchWidth_limit", StringType(), True),
-            StructField("StitchArea_limit", StringType(), True),
-            StructField("LeakTest_limit", StringType(), True)
-        ])
-
-        # Convert Pandas DataFrame to Spark DataFrame using the defined schema
-        spark_df = spark.createDataFrame(df, schema=schema)
+        # Convert Pandas DataFrame to Spark DataFrame
+        spark_df = spark.createDataFrame(df)
 
         # Rename BatchID column to _BatchID
         spark_df = spark_df.withColumnRenamed("BatchID", "_BatchID")
@@ -121,8 +47,24 @@ def spark_refine(df):
         remove_cols = ["_batchid", "timestamp"]
         spark_df = spark_df.drop(*remove_cols)
 
-        # Save refined DataFrame as Parquet file locally
-        spark_df.write.parquet(local_path_refine_output, mode="overwrite")
+        # Save refined DataFrame as JSON file locally
+        spark_df.write.json(local_path_refine_output)
+
+        spark.stop()
+
+        return True
+    except Exception as e:
+        print(f"An error occurred in spark_refine: {str(e)}")
+        return False
+
+
+def spark_anomaly(df):
+    try:
+        # Create a SparkSession
+        spark = SparkSession.builder.appName("Local_write_and_query").getOrCreate()
+
+        # Convert Pandas DataFrame to Spark DataFrame
+        spark_df = spark.createDataFrame(df)
 
         # Drop unnecessary columns
         remove_col = [
@@ -134,40 +76,59 @@ def spark_refine(df):
         # Handle missing values by replacing them with a specific value (-999)
         spark_df = spark_df.fillna(-999)
 
-        # Save anomaly DataFrame as Parquet file locally
-        spark_df.write.parquet(local_path_anomaly_output, mode="overwrite")
-
-        # Query and save grouped DataFrame
-        query_week_grouped = """
-            SELECT week,
-                COUNT(*) AS count_of_records,
-                MAX(domecasegap) AS maximum_domecasegap,
-                MIN(domecasegap) AS minimum_domecasegap,
-                MIN(stitcharea) AS minimum_stitcharea,
-                AVG(domecasegap) AS week_mean,
-                STDDEV(domecasegap) AS week_stddev,
-                AVG(stitcharea) AS stitcharea_week_mean,
-                STDDEV(stitcharea) AS stitcharea_week_stddev
-            FROM raw_table
-            GROUP BY week
-        """
-        grouped_df = spark.sql(query_week_grouped)
-        grouped_df.write.parquet(local_path_weeks_raws_output, mode="overwrite")
+        # Save anomaly DataFrame as JSON file locally
+        spark_df.write.json(local_path_anomaly_output)
 
         spark.stop()
 
         return True
     except Exception as e:
-        print(f"An error occurred at local_to_refine_hdfs.py: {str(e)}")
+        print(f"An error occurred in spark_anomaly: {str(e)}")
         return False
+
+
+def spark_weekly_rows(df):
+    try:
+        # Create a SparkSession
+        spark = SparkSession.builder.appName("Local_write_and_query").getOrCreate()
+
+        # Convert Pandas DataFrame to Spark DataFrame
+        spark_df = spark.createDataFrame(df)
+
+        # Query and save grouped DataFrame
+        grouped_df = spark_df.groupby("week").agg(
+            F.count("*").alias("count_of_records"),
+            F.max("domecasegap").alias("maximum_domecasegap"),
+            F.min("domecasegap").alias("minimum_domecasegap"),
+            F.min("stitcharea").alias("minimum_stitcharea"),
+            F.avg("domecasegap").alias("week_mean"),
+            F.stddev("domecasegap").alias("week_stddev"),
+            F.avg("stitcharea").alias("stitcharea_week_mean"),
+            F.stddev("stitcharea").alias("stitcharea_week_stddev")
+        )
+
+        # Save grouped DataFrame as JSON file locally
+        grouped_df.write.json(local_path_weeks_raws_output)
+
+        spark.stop()
+
+        return True
+    except Exception as e:
+        print(f"An error occurred in spark_weekly_rows: {str(e)}")
+        return False
+
 
 if __name__ == '__main__':
     try:
-        # Read raw DataFrame from Parquet file using PyArrow
-        scd_raw = pq.read_table('/home/naya/anomaly/anomaly/sealing_anomaly/files_parquet/scd_raw.parquet').to_pandas()
+        # Read raw DataFrame from JSON file
+        scd_raw = spark.read.json('/home/naya/anomaly/files_json/scd_raw.json').toPandas()
         print(scd_raw.head())
-        success = spark_refine(scd_raw)
-        if success:
+
+        success_refine = spark_refine(scd_raw)
+        success_anomaly = spark_anomaly(scd_raw)
+        success_weekly_rows = spark_weekly_rows(scd_raw)
+
+        if success_refine and success_anomaly and success_weekly_rows:
             print("Files are ready")
         else:
             print("ERRORS FOUND!")
