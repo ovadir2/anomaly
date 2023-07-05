c9c641669aa063b3da470bfe111b3af8e205c459
ovadir2 <ovadir2@gmail.com>
2023-07-05 14:04:07 +0000

    1

--- airflow_anomaly_dag.py
+++ airflow_anomaly_dag.py
@@ -13,19 +13,18 @@ dag = DAG(
     'parallel_tasks',
     default_args=default_args,
     start_date=datetime(2023, 7, 3),
-    schedule_interval=timedelta(days=1)  # Daily schedule
+    schedule=timedelta(days=1)  # Daily schedule
 )
 
 task3 = BashOperator(
-    task_id='task_3',
+    task_id='task_1',
     bash_command='/home/naya/miniconda3/envs/airflow/bin/python /home/naya/anomaly/producer_get_raw_data.py --history=no',
-    dag=dag,
-    execution_date_fn=lambda dt: dt.replace(hour=0, minute=5)  # Task3 will run every day at 00:05 AM
+    dag=dag
 )
 
 task4 = BashOperator(
-    task_id='task_4',
+    task_id='task_2',
     bash_command='/home/naya/miniconda3/envs/airflow/bin/python /home/naya/anomaly/prediction.py --MIN_PRED_RECORD=20 --NEXT_TRAIN_QTY=200',
     dag=dag,
-    execution_date_fn=lambda dt: dt.replace(hour=0, minute=5, weekday=4)  # Task4 will run weekly on Friday at 00:05 AM
+    schedule=timedelta(days=7)  # Weekly schedule
 )
